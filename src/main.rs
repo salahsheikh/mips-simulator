27daf2d728be046ec6b70c796e9f06229c6f851e
Salah <salah.h.sheikh@gmail.com>
2019-03-23 20:38:24 -0600

    Add cli argument passing

--- src/main.rs
+++ src/main.rs
@@ -5,10 +5,22 @@ use std::io::BufRead;
 pub mod architecture;
 pub mod parser;
 
+extern crate clap;
+use clap::{Arg, App};
+
 fn main() {
+    let matches = App::new("MIPS Simulator")
+        .version("0.1")
+        .author("Salah Sheikh <salahsheikh AT outlook DOT com>")
+        .arg(Arg::with_name("input")
+            .help("Sets the input file to use")
+            .required(true)
+            .index(1))
+        .get_matches();
+
     let mut proc: architecture::Processor = architecture::Processor::new();
 
-    let f = File::open("./src.asm");
+    let f = File::open(matches.value_of("input").unwrap());
     let f = match f {
         Ok(file) => file,
         Err(error) => {
